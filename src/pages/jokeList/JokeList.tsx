b0da6f668131fff0aa651b8caa387860ff49bf06
Parisa Safari <safari.pary@gmail.com>
2023-05-20 19:36:45 +0200

    Added fetch timer for updating the joke list

--- src/pages/jokeList/JokeList.tsx
+++ src/pages/jokeList/JokeList.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react'
 
+const URL = 'https://api.chucknorris.io/jokes/random'
+
 interface ChuckNorrisJoke {
   icon_url: string
   id: string
@@ -9,19 +11,51 @@ interface ChuckNorrisJoke {
 
 export const JokeList = () => {
   const [jokes, setJokes] = useState<ChuckNorrisJoke[]>([])
+  const [timer, setTimer] = useState<boolean>(false)
+
+  const fetchNewJoke = async () => {
+    try {
+      const response = await fetch(URL)
+      const data = await response.json()
+      setJokes(prevJokes => {
+        if (prevJokes.length === 10) {
+          prevJokes.shift()
+        }
+        return [...prevJokes, data]
+      })
+    } catch (error) {
+      console.error('Error fetching new joke:', error)
+    }
+  }
 
   useEffect(() => {
-    fetch('https://api.chucknorris.io/jokes/random', {})
-      .then(response => {
-        return response.json()
-      })
-      .then((data: ChuckNorrisJoke) => {
-        const newJoke = [...jokes, data]
-        console.log('---', newJoke)
-        setJokes(newJoke)
+    Promise.all(
+      Array(10)
+        .fill(URL)
+        .map(url => {
+          return fetch(url).then(response => response.json())
+        }),
+    )
+      .then((data: ChuckNorrisJoke[]) => {
+        setJokes(data)
+        setTimer(true)
       })
       .catch(console.error)
   }, [])
 
-  return <div>this is a joke list page: {jokes?.[0]?.value}</div>
+  useEffect(() => {
+    const timer = setInterval(fetchNewJoke, 5000)
+    return () => {
+      clearInterval(timer)
+    }
+  }, [timer])
+
+  return (
+    <div>
+      this is a joke list page:
+      {jokes?.map(item => (
+        <p key={item.id}>{item.value}</p>
+      ))}
+    </div>
+  )
 }
