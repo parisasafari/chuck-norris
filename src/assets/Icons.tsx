990df4c1f11169401b7afa9bc9cb4c4a73a926da
Parisa Safari <safari.pary@gmail.com>
2023-05-21 21:59:23 +0200

    Added loader icon

---
+++ src/assets/Icons.tsx
@@ -0,0 +1,78 @@
+const Icons = {
+  loading: () => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="200px"
+      height="200px"
+      viewBox="0 0 100 100"
+      preserveAspectRatio="xMidYMid"
+    >
+      <rect x="15" y="35" width="20" height="30" fill="#fef1c0">
+        <animate
+          attributeName="y"
+          repeatCount="indefinite"
+          dur="1s"
+          calcMode="spline"
+          keyTimes="0;0.5;1"
+          values="33.5;35;35"
+          keySplines="0 0.5 0.5 1;0 0.5 0.5 1"
+          begin="-0.2s"
+        ></animate>
+        <animate
+          attributeName="height"
+          repeatCount="indefinite"
+          dur="1s"
+          calcMode="spline"
+          keyTimes="0;0.5;1"
+          values="33;30;30"
+          keySplines="0 0.5 0.5 1;0 0.5 0.5 1"
+          begin="-0.2s"
+        ></animate>
+      </rect>
+      <rect x="40" y="35" width="20" height="30" fill="#ffd3d6">
+        <animate
+          attributeName="y"
+          repeatCount="indefinite"
+          dur="1s"
+          calcMode="spline"
+          keyTimes="0;0.5;1"
+          values="33.875;35;35"
+          keySplines="0 0.5 0.5 1;0 0.5 0.5 1"
+          begin="-0.1s"
+        ></animate>
+        <animate
+          attributeName="height"
+          repeatCount="indefinite"
+          dur="1s"
+          calcMode="spline"
+          keyTimes="0;0.5;1"
+          values="32.25000000000001;30;30"
+          keySplines="0 0.5 0.5 1;0 0.5 0.5 1"
+          begin="-0.1s"
+        ></animate>
+      </rect>
+      <rect x="65" y="35" width="20" height="30" fill="#c0b9bf">
+        <animate
+          attributeName="y"
+          repeatCount="indefinite"
+          dur="1s"
+          calcMode="spline"
+          keyTimes="0;0.5;1"
+          values="33.875;35;35"
+          keySplines="0 0.5 0.5 1;0 0.5 0.5 1"
+        ></animate>
+        <animate
+          attributeName="height"
+          repeatCount="indefinite"
+          dur="1s"
+          calcMode="spline"
+          keyTimes="0;0.5;1"
+          values="32.25000000000001;30;30"
+          keySplines="0 0.5 0.5 1;0 0.5 0.5 1"
+        ></animate>
+      </rect>
+    </svg>
+  ),
+}
+
+export default Icons
